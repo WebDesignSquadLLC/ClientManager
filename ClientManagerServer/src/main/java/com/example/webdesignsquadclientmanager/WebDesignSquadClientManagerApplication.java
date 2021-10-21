b0f80a1859c5e6c52c616fa029161d379c3fafc7
ChristianManu <manuc@mymail.vcu.edu>
2021-10-21 18:08:00 -0400

    restard first commit

--- ClientManagerServer/src/main/java/com/example/webdesignsquadclientmanager/WebDesignSquadClientManagerApplication.java
+++ ClientManagerServer/src/main/java/com/example/webdesignsquadclientmanager/WebDesignSquadClientManagerApplication.java
@@ -2,6 +2,8 @@ package com.example.webdesignsquadclientmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class WebDesignSquadClientManagerApplication {
