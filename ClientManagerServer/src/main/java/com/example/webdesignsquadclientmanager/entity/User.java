7f6ac362f3728d70039608255728b48c325afe1f
ChristianManu <manuc@mymail.vcu.edu>
2021-10-22 07:55:13 -0400

    added role entities with corresponding repositories

--- ClientManagerServer/src/main/java/com/example/webdesignsquadclientmanager/entity/User.java
+++ ClientManagerServer/src/main/java/com/example/webdesignsquadclientmanager/entity/User.java
@@ -5,7 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Getter
 @Setter
@@ -35,6 +39,14 @@ public class User {
     @Column(name = "password")
     private String password;
 
+    @Column(name="user_role")
+    private String user_role;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
 
 }
